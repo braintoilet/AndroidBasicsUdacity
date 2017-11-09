5c50f14f7c100d9d0e47367325b4c1694a3bee93
braintoilet <jerome.grassnick@gmail.com>
2017-11-09 18:43:40 +0100

    Updated JustJava Project with changes made in Lesson 7

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -51,7 +51,15 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
+        String priceMessage = "Total: $" + number + "\nThank You!";
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText(message);
     }
 }
